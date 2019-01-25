9230de3fd3248dc15811f5bf3543684cc649755d
Adam Kaufman <adam@abacusinsights.com>
2019-01-24 20:49:36 -0500

    added 3.7 to travis, updated classifiers

--- setup.py
+++ setup.py
@@ -23,16 +23,15 @@ setup(
         'decorator==4.2.1'
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable  ',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
